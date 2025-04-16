ac919a702a013aeca2cbc3fb803c65bc09e8554c
Edwins247 <cheewr85@daum.net>
2025-04-16 11:25:48 +0900

    webpack 설정 및 세팅

--- Practice/withoutCRA/src/components/FollowButton.js
+++ Practice/withoutCRA/src/components/FollowButton.js
@@ -1,55 +1,56 @@
+import React from "react";
 // React Component!
 // Follow <-> following
 function FollowButton() {
-    // Follow 상태를 위해 state 선언 및 저장
-    const [following, setFollowing] = React.useState(false);
-  
-    // 스타일링 추가
-    const commonBtnStyle = {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      position: "absolute",
-      top: "12px",
-      right: "16px",
-      width: "100px",
-      height: "36px",
-      borderRadius: "9999px",
-      fontWeight: "bold",
-    };
-  
-    const followBtnStyle = {
-      ...commonBtnStyle,
-      backgroundColor: "black",
-      color: "white",
-    }
-  
-    const followingBtnStyle = {
-      ...commonBtnStyle,
-      backgroundColor: "white",
-      color: "black",
-      border: "1px solid #cfd9de",
-    }
-  
-    // JavaScript로 DOM을 생성하는 방법
-    // return React.createElement(
-    //   "div",
-    //   {
-    //     onClick: () => setFollowing(!following),
-    //     style: following ? followingBtnStyle : followBtnStyle,
-    //   },
-    //   following ? "following" : "Follow"
-    // );
+  // Follow 상태를 위해 state 선언 및 저장
+  const [following, setFollowing] = React.useState(false);
 
-    // JSX로 DOM을 생성하는 방법
-    return (
-        <div
-            onClick={() => setFollowing(!following)}
-            style={following ? followingBtnStyle : followBtnStyle}
-        >
-            {following ? "following" : "Follow"}
-        </div>
-    )
-  }
+  // 스타일링 추가
+  const commonBtnStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: "12px",
+    right: "16px",
+    width: "100px",
+    height: "36px",
+    borderRadius: "9999px",
+    fontWeight: "bold",
+  };
 
-  
+  const followBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "black",
+    color: "white",
+  };
+
+  const followingBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "white",
+    color: "black",
+    border: "1px solid #cfd9de",
+  };
+
+  // JavaScript로 DOM을 생성하는 방법
+  // return React.createElement(
+  //   "div",
+  //   {
+  //     onClick: () => setFollowing(!following),
+  //     style: following ? followingBtnStyle : followBtnStyle,
+  //   },
+  //   following ? "following" : "Follow"
+  // );
+
+  // JSX로 DOM을 생성하는 방법
+  return (
+    <div
+      onClick={() => setFollowing(!following)}
+      style={following ? followingBtnStyle : followBtnStyle}
+    >
+      {following ? "following" : "Follow"}
+    </div>
+  );
+}
+
+export default FollowButton;
