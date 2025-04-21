d0d63058b34ef33900d1fb90dabec04baeaefbcf
Edwins247 <cheewr85@daum.net>
2025-04-21 15:35:33 +0900

    React Query & Suspense 실습

--- Practice/my-store/src/pages/Edit.js
+++ Practice/my-store/src/pages/Edit.js
@@ -1,25 +1,69 @@
 import React, { useState } from "react";
+import { useQuery, useMutation, useQueryClient, useQueries } from "react-query";
+import { getPosts, getUser, updateNickname } from "../mocks/api";
+
+// api를 통해 현재 닉네임값 가져오기
+// handleSubmit: 업데이트 된 inputValue를 서버에 전송해서, 닉네임값 업데이트하기
 
 export default function Edit() {
   const [inputValue, setInputValue] = useState("");
+  const queryClient = useQueryClient();
+
+  // const { data : user } = useQuery('@getUser', getUser, {
+  //   staleTime: Infinity
+  // });
+
+  // const { data : posts } = useQuery('@getPosts', getPosts, {
+  //   staleTime: Infinity
+  // });
+
+  const results = useQueries([
+    {
+      queryKey: ['@getUser'],
+      queryFn: getUser,
+      staleTime: Infinity
+    },
+    {
+      queryKey: ['@getPosts'],
+      queryFn: getPosts,
+      staleTime: Infinity
+    },
+  ]);
+
+  const user = results[0].data;
+  const posts = results[1].data;
+
+  const mutation = useMutation(updateNickname, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('@getUser');
+    }
+  });
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    mutation.mutate(inputValue);
   };
+
+  // if (isLoading) return <span>Loading...</span>
 
   return (
     <>
       <h1>Edit</h1>
-      <h3>현재 닉네임: {"hwarari"}</h3>
+      <h3>현재 닉네임: {user?.nickName}</h3>
       <form onSubmit={handleSubmit}>
         <label>
           변경할 닉네임:
           <input type="text" value={inputValue} onChange={handleChange} />
         </label>
       </form>
+      <ul>
+        {posts.map((post) => (
+          <li>{post?.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
