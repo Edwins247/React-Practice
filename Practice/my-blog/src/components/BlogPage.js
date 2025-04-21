900e2b670bd849745caf26098f0372b9f917d9ea
Edwins247 <cheewr85@daum.net>
2025-04-21 11:53:04 +0900

    useReducer 실습

--- Practice/my-blog/src/components/BlogPage.js
+++ Practice/my-blog/src/components/BlogPage.js
@@ -2,10 +2,13 @@ import React, { useContext } from "react";
 import { UserContext } from "../store/user";
 
 export default function BlogPage() {
-  const value = useContext(UserContext);
+  const dispatch = useContext(UserContext);
+
+
   return (
     <div>
       <h1>BlogPage</h1>
+      <button onClick={() => dispatch({ type: "changeJob", text: "BE-developer"})}>Change Job</button>
     </div>
   );
 }
--- Practice/my-blog/src/store/user.js
+++ Practice/my-blog/src/store/user.js
@@ -1,15 +1,35 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useReducer } from "react";
 
 export const UserContext = createContext();
 
+const initialUser = {
+    name: "hwarari",
+    job: "FE-developer",
+}
+
+const userReducer = (state, action) => {
+    switch (action.type) {
+        case "changeJob":
+            // state의 job에 해당하는 데이터를 action.text
+            return { ...state, job: action.text }
+            break;
+        
+        default:
+            break;
+    }
+}
+
 export default function UserStore(props) {
-    const [job, setJob] = useState("FE-developer");
+    // const [job, setJob] = useState("FE-developer");
 
-    const user = {
-        name: "hwarari",
-        job: "FE-developer",
-        changeJob: (updatedJob) => setJob(updatedJob),
-    };
+    const [user, dispatch] = useReducer(userReducer, initialUser);
+    console.log(user);
 
-    return <UserContext.Provider value={user}>{props.children}</UserContext.Provider>
+    // const user = {
+    //     name: "hwarari",
+    //     job: "FE-developer",
+    //     changeJob: (updatedJob) => setJob(updatedJob),
+    // };
+
+    return <UserContext.Provider value={dispatch}>{props.children}</UserContext.Provider>
 }
