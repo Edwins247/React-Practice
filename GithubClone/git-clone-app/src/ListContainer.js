792abc9b988df8d25522507bbf5de60bb5f82180
Edwins247 <cheewr85@daum.net>
2025-04-22 19:21:39 +0900

    Github Issue 페이지 UI 제작

---
+++ GithubClone/git-clone-app/src/ListContainer.js
@@ -0,0 +1,135 @@
+import styles from "./ListContainer.module.css";
+import Button from "./components/Button";
+import ListItem from "./components/ListItem";
+import cx from "clsx";
+
+import { useState } from "react";
+import ListItemLayout from "./components/ListItemLayout";
+import Modal from "./components/Modal";
+import Pagination from "./components/Pagination";
+
+export default function ListContainer() {
+  const [inputValue, setInputValue] = useState("is:pr is:open");
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  return (
+    <>
+      <div className={styles.listContainer}>
+        <div className={styles.topSection}>
+          <input
+            className={styles.input}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button
+            style={{
+              fontSize: "14px",
+              backgroundColor: "green",
+              color: "white",
+            }}
+          >
+            New Issue
+          </Button>
+        </div>
+        <OpenClosedFilters />
+        <ListItemLayout className={styles.listFilter}>
+          <ListFilter onChangeFilter={(filteredData) => {}} />
+        </ListItemLayout>
+        <div className={styles.container}>
+          {list.map((listItem, index) => (
+            <ListItem
+              key={index}
+              badges={[
+                {
+                  color: "red",
+                  title: "Bug",
+                },
+              ]}
+            />
+          ))}
+        </div>
+      </div>
+      <div className={styles.paginationContainer}>
+      <Pagination
+        maxPage={10}
+        currentPage={page}
+        onClickPageButton={(number) => setPage(number)}
+      />
+      </div>
+    </>
+  );
+}
+
+function ListFilter({ onChangeFilter }) {
+  return (
+    <>
+      <div className={styles.filterLists}>
+        <ListFilterItem>Author</ListFilterItem>
+        <ListFilterItem>Label</ListFilterItem>
+        <ListFilterItem>Projects</ListFilterItem>
+        <ListFilterItem>Milestone</ListFilterItem>
+        <ListFilterItem>Assignee</ListFilterItem>
+        <ListFilterItem>Sort</ListFilterItem>
+      </div>
+    </>
+  );
+}
+
+function ListFilterItem({ onClick, children, onChangeFilter }) {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div className={styles.filterItem}>
+      <span role="button" onClick={() => setShowModal(true)}>
+        {children} ▾
+      </span>
+      <div className={styles.modalContainer}>
+        <Modal
+          opened={showModal}
+          onClose={() => setShowModal(false)}
+          placeholder="Filter labels"
+          searchDataList={["bug", "Labels", "Apple"]}
+          onClickCell={() => {
+            onChangeFilter();
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function OpenClosedFilters({ data }) {
+  const [isOpenMode, setIsOpenMode] = useState(true);
+
+  const openModeDataSize = 1;
+  const closeModeDataSize = 2;
+  return (
+    <>
+      <OpenClosedFilter
+        size={openModeDataSize}
+        state="Open"
+        onClick={() => setIsOpenMode(true)}
+        selected={isOpenMode}
+      />
+      <OpenClosedFilter
+        size={closeModeDataSize}
+        state="Closed"
+        onClick={() => setIsOpenMode(false)}
+        selected={!isOpenMode}
+      />
+    </>
+  );
+}
+
+function OpenClosedFilter({ size, state, onClick, selected }) {
+  return (
+    <span
+      role="button"
+      className={cx(styles.textFilter, { [styles.selected]: selected })}
+      onClick={onClick}
+    >
+      {size} {state}
+    </span>
+  );
+}
